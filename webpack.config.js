7c3f4841f8a0d15bbbdda259c3e3f9f37b37db2b
Hoa Nguyen <nvh0412@gmail.com>
2016-07-09 10:01:26 +0700

    [c] configure clean, seperate bundle and webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,17 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const validate = require('webpack-validator');
+const parts = require('./libs/parts');
 
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
 };
 
-module.exports = {
+const common = {
   entry: {
-    app: PATHS.app
+    app: PATHS.app,
   },
   output: {
     path: PATHS.build,
@@ -19,4 +22,49 @@ module.exports = {
       title: 'Vagabond'
     })
   ]
+};
+
+var config;
+
+//Detect how npm is run and branch based on that
+switch (process.env.npm_lifecycle_event) {
+  case 'build':
+    config = merge(common,
+      {
+        devtool: 'source-map',
+        output: {
+          path: PATHS.build,
+          filename: '[name].[chunkhash].js',
+          // This is used for require.ensure. The setup
+          // will work without but this is useful to set.
+          chunkFilename: '[chunkhash].js'
+        }
+      },
+      parts.setFreeVariable(
+        'process.env.NODE_ENV',
+        'production'
+      ),
+      parts.extractBundle({
+        name: 'vendor',
+        entries: ['react']
+      }),
+      parts.minify(),
+      parts.setupCSS(PATHS.app)
+    );
+    break;
+  case 'stats':
+  default:
+    config = merge(
+      common,
+      {
+        devtool: 'eval-source-map'
+      },
+      parts.setupCSS(PATHS.app),
+      parts.devServer({
+        host: process.env.HOST,
+        port: process.env.PORT
+      })
+    );
 }
+
+module.exports = validate(config);
